3cda397a0a8ce87470f0cc79a9420e5c273a9025
= <abhisingh30800@gmail.com>
2023-11-09 12:16:24 +0530

    added more methods for stacks & queues

--- Stacks-&-Queues/index.js
+++ Stacks-&-Queues/index.js
@@ -24,8 +24,69 @@ class Stack {
         this.length++;
         return this;
     }
+    pop() {
+        if (this.length == 0) return undefined;
+        let temp = this.top;
+        if (this.length === 1) {
+            this.top = null;
+        }
+        else {
+            this.top = this.top.next;
+            temp.next = null;
+        }
+        this.length--;
+        return temp;
+    }
 }
 
-const stack1 = new Stack(4);
-stack1.push(77);
-console.log(stack1);
+
+class Queue {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.first = newNode;
+        this.last = newNode;
+        this.length = 1;
+    }
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        }
+        else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    dequeue() {
+        if (!this.first) return undefined;
+        let temp = this.first;
+        if (this.length === 1) {
+            this.first = null;
+            this.last = null;
+        }
+        else {
+            this.first = this.first.next;
+            temp.next = null;
+        }
+        this.length--;
+        return temp;
+    }
+}
+
+// const stack1 = new Stack(4);
+// stack1.push(77);
+// stack1.push(10);
+// console.log(stack1.pop());
+// console.log(stack1);
+
+
+const queue1 = new Queue(10);
+queue1.enqueue(7);
+queue1.enqueue(20);
+queue1.dequeue();
+queue1.dequeue();
+queue1.dequeue();
+console.log(queue1);
