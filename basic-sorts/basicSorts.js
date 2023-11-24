92a685ce44bcaf35850c6b565d65c9b9d81e1a75
= <abhisingh30800@gmail.com>
2023-11-24 12:09:09 +0530

    added merge sort

--- basic-sorts/basicSorts.js
+++ basic-sorts/basicSorts.js
@@ -1,28 +1,26 @@
-function bubbleSort(array) {
-    for (var i = array.length - 1; i > 0; i--) {
+function bubbleSort(arr) {
+    for (var i = arr.length - 1; i > 0; i--) {
         for (var j = 0; j < i; j++) {
-            if (array[j] > array[j + 1]) {
-                let temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    return array;
+    return arr;
 }
-
-
 
 
 function selectionSort(arr) {
     for (var i = 0; i < arr.length; i++) {
         let min = i;
-        for (var j = i + 1; j < arr.length; j++) {
+        for (j = i + 1; j < arr.length; j++) {
             if (arr[min] > arr[j]) {
                 min = j;
             }
         }
-        if (i !== min) {
+        if (min !== i) {
             let temp = arr[i];
             arr[i] = arr[min];
             arr[min] = temp;
@@ -37,14 +35,42 @@ function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
         temp = arr[i];
         for (var j = i - 1; arr[j] > temp && j > -1; j--) {
-            arr[j + 1] = arr[j];
+            arr[j + 1] = arr[j]
         }
         arr[j + 1] = temp;
     }
     return arr;
 }
 
-const arr1 = [4, 2, 34, 6, 100, 1];
-// console.log(bubbleSort(arr1));
-// console.log(selectionSort(arr1));
-console.log(insertionSort(arr1));
+const arr = [44, 1, 99, 10, 2];
+
+// console.log(bubbleSort(arr));
+// console.log(selectionSort(arr));
+console.log(insertionSort(arr));
+
+
+function checkAmount() {
+    var abhiAccount = {
+        name: "Abhishek",
+        amount: 4000,
+        deductAmount: function (amount) {
+            this.amount -= amount;
+            return this.amount;
+        }
+    }
+
+
+    var prabAccount = {
+        name: "Prabh Gill",
+        amount: 6000,
+    }
+
+
+    var totalAmount = function (account, amount) {
+        return abhiAccount.deductAmount.call(account, amount);
+    }
+    console.log(totalAmount(prabAccount, 500));
+}
+
+
+checkAmount();
