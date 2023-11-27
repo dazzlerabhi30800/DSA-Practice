a4d2cb742bbcce64e2d63bcd9d885242a944263f
= <abhisingh30800@gmail.com>
2023-11-27 19:07:23 +0530

    added tree traversal breadth first search method

---
+++ tree-traversal/index.js
@@ -0,0 +1,85 @@
+console.log("Tree Traversal");
+
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null
+        this.right = null;
+    }
+}
+
+
+class BST {
+    constructor() {
+        this.root = null
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return true;
+        }
+
+        let temp = this.root;
+        while (true) {
+            if (temp.value > newNode.value) {
+                if (!temp.left) {
+                    temp.left = newNode;
+                    return this;
+                }
+                temp = temp.left;
+            }
+            else if (temp.value < newNode.value) {
+                if (!temp.right) {
+                    temp.right = newNode;
+                    return this;
+                }
+                temp = temp.right;
+            }
+            else {
+                return this;
+            }
+        }
+    }
+    contains(value) {
+        if (!this.root) return undefined;
+        let temp = this.root;
+        while (temp) {
+            if (temp.value < value) {
+                temp = temp.right;
+            }
+            else if (temp.value > value) {
+                temp = temp.left;
+            }
+            else {
+                return temp;
+            }
+        }
+    }
+    // Breadth First Search
+    BFS() {
+        let currentNode = this.root;
+        let queue = [];
+        let results = [];
+        queue.push(currentNode);
+        while (queue.length) {
+            currentNode = queue.shift();
+            results.push(currentNode.value);
+            if (currentNode.left) queue.push(currentNode.left);
+            if (currentNode.right) queue.push(currentNode.right);
+        }
+
+        return results;
+    }
+}
+
+
+const myTree = new BST();
+myTree.insert(7);
+myTree.insert(8);
+myTree.insert(10);
+myTree.insert(5);
+myTree.insert(6);
+console.log(myTree.BFS());
