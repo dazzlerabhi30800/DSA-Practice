735d1b912f38e629b6aef717f51ceacb7934a3c5
= <abhisingh30800@gmail.com>
2023-12-10 20:47:35 +0530

    added all functionality in 25 + 5 clock

---
+++ FreeCodeCamp-Frontend/Projects/25+5-Clock/main.js
@@ -0,0 +1,103 @@
+let breakLength = 1;
+let sessionLength = 1;
+
+let start = false;
+let breakStart = false;
+
+
+let minutes = sessionLength;
+let seconds = 0;
+
+
+var myInterval;
+
+
+$("document").ready(function () {
+    $("#session-length").text(sessionLength);
+    $("#break-length").text(breakLength);
+    $("#time-left").text(`${minutes <= 0 ? "0" + minutes : minutes}:${seconds <= 0 ? "0" + seconds : seconds}`);
+})
+
+
+
+$("#start_stop").click(function () {
+    start = start ? false : true;
+    if (start) {
+        $("#bell").trigger("pause");
+        $("#bell")[0].currentTime = 0;
+        $(this).children().removeClass('fa-play');
+        $(this).children().addClass('fa-pause');
+        myInterval = setInterval(() => {
+            seconds--;
+            if (seconds <= 0 && minutes > 0) {
+                seconds = 59;
+                minutes -= 1;
+            }
+            if (seconds < 0 && minutes === 0 && !breakStart) {
+                console.log("times up");
+                minutes = breakLength;
+                breakStart = true;
+                $("#timer-label").text("Break");
+                $("#bell").trigger("play");
+                seconds = 0;
+            }
+            if (seconds < 0 && minutes === 0 && breakStart) {
+                breakStart = false;
+                clearInterval(myInterval);
+                $("#timer-label").text("Session");
+                minutes = sessionLength;
+                seconds = 0;
+            }
+            $('#time-left').text(`${minutes < 10 ? "0" + minutes : minutes}: ${seconds < 10 ? "0" + seconds : seconds} `)
+        }, 1000)
+    }
+    else {
+        clearInterval(myInterval);
+        $(this).children().removeClass('fa-pause');
+        $(this).children().addClass('fa-play');
+    }
+})
+
+
+$("#reset").click(function () {
+    start = false;
+    clearInterval(myInterval);
+    minutes = sessionLength;
+    seconds = 0;
+    $("#start_stop").children().removeClass('fa-pause');
+    $("#start_stop").children().addClass('fa-play');
+    $('#time-left').text(`${minutes < 10 ? "0" + minutes : minutes}: ${seconds < 10 ? "0" + seconds : seconds} `)
+})
+
+
+
+$("#break-decrement").click(function () {
+    if (start) return;
+    breakLength = breakLength - 1 > 0 ? breakLength - 1 : 1;
+    $("#break-length").text(breakLength);
+})
+
+$("#break-increment").click(function () {
+    if (start) return;
+    breakLength = breakLength + 1 > 60 ? 59 : breakLength + 1;
+    $("#break-length").text(breakLength);
+})
+
+$("#session-decrement").click(function () {
+    if (start) return;
+    sessionLength = sessionLength - 1 > 0 ? sessionLength - 1 : 1;
+    minutes = sessionLength;
+    $("#session-length").text(sessionLength);
+    $('#time-left').text(`${minutes < 10 ? "0" + minutes : minutes}: ${seconds < 10 ? "0" + seconds : seconds} `)
+})
+
+
+
+$("#session-increment").click(function () {
+    if (start) return;
+    sessionLength = sessionLength + 1 > 60 ? 59 : sessionLength + 1;
+    minutes = sessionLength;
+    $("#session-length").text(sessionLength);
+    $('#time-left').text(`${minutes < 10 ? "0" + minutes : minutes}: ${seconds < 10 ? "0" + seconds : seconds} `)
+})
+console.log(minutes);
